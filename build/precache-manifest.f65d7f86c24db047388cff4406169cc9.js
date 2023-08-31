223da5a6d273f1fa21394fe24d2df0203be75148
Ashwanikrs <ashwaniksingh111@gmail.com>
2023-08-31 20:49:08 +0530

    first commit

---
+++ build/precache-manifest.f65d7f86c24db047388cff4406169cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5fc7ef0b87d5c2790b",
+    "url": "/Ashwanikrs/Rzzum/static/js/main.9f5fc7ef.chunk.js"
+  },
+  {
+    "revision": "3534e9634678190a58cc",
+    "url": "/Ashwanikrs/Rzzum/static/js/1.3534e963.chunk.js"
+  },
+  {
+    "revision": "49cd9370f5281e344855",
+    "url": "/Ashwanikrs/Rzzum/static/js/runtime~main.49cd9370.js"
+  },
+  {
+    "revision": "9b6467244dbc9ad41e370f70f4b67109",
+    "url": "/Ashwanikrs/Rzzum/static/media/example_photo.9b646724.jpg"
+  },
+  {
+    "revision": "d9396cc0df9bfdfa504e526392489d57",
+    "url": "/Ashwanikrs/Rzzum/static/media/empty_avatar.d9396cc0.png"
+  },
+  {
+    "revision": "3267b96f4d9f14d622a812bfd3997776",
+    "url": "/Ashwanikrs/Rzzum/index.html"
+  }
+];
